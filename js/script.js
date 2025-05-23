abff3f8158495bf49d38160e67d81fbcaa8a5c3a
OshixFFyt <monkeyking7480@gmail.com>
2025-05-23 13:48:28 +0500

    simple prog

--- js/script.js
+++ js/script.js
@@ -1,7 +1,31 @@
 {
-    let a,b,mul;
+    let a, b, mul;
         a = 8;
         b = 9;
         mul= a*b;
         console.log(mul);
+}
+
+// {
+//     let number , page , numberOfPage
+//     number = parseInt(prompt("Введите количество фото в папке " , 10))
+//     page = parseInt (prompt("Введите количество фото на одной странице " , 10))
+//     numberOfPage = Math.ceil (number / page)
+//     alert("ответ " + numberOfPage)
+// }
+
+
+{
+    let a = 1, b;
+     b = a++;
+    console.log(a, b)
+}
+
+{
+let number , d1 , d2 , d3 , d4;
+number = parseInt(prompt("Введите число "))
+d1 = Math.floor(number / 1) % 10
+d2 = Math.floor(number / 10) % 10
+d3 = Math.floor(number / 100) % 10
+d4 = Math.floor (number / 1000)% 10
 }
